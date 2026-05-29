0253b7e8b90ec46e81b203b14c77ce3012a98c5a
Roberto <rc7494820@gmail.com>
2026-05-28 21:08:19 -0500

    componentes de recetas

---
+++ Recetas/src/app/components/lista-recetas/lista-recetas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-recetas',
+  imports: [],
+  templateUrl: './lista-recetas.html',
+  styleUrl: './lista-recetas.css',
+})
+export class ListaRecetas {}
